97f2af87a9fe3bb05b8691048c0062d8e4d5c46f
siddhant saxena <sid2507@gmail.com>
2023-07-17 02:29:05 +0530

    Procurement Form Creation

---
+++ src/app/asset-request-form/asset-request-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-request-form',
+  templateUrl: './asset-request-form.component.html',
+  styleUrls: ['./asset-request-form.component.css']
+})
+export class AssetRequestFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
