b1074719de63dad8f9add4b1d196ecca04617b23
siddhant saxena <sid2507@gmail.com>
2023-07-17 21:19:07 +0530

    Modification in the form

--- src/app/asset-request-form/asset-request-form.component.ts
+++ src/app/asset-request-form/asset-request-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-asset-request-form',
@@ -12,4 +13,41 @@ export class AssetRequestFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  approvedMachine: string | undefined;
+  companyName: string | undefined;
+  location: string | undefined;
+  subLocation: string | undefined;
+
+  assetOwnerFirstName: string | undefined;
+  assetOwnerMiddleName: string | undefined;
+  assetOwnerLastName: string | undefined;
+  assetOwnerEmail: string | undefined;
+  edpNumber: string | undefined;
+  assetOwnerDesignation: string | undefined;
+  costCenter: string | undefined;
+  department: string | undefined;
+
+  organizationName: string | undefined;
+  flatBuilding: string | undefined;
+  streetName: string | undefined;
+  city: string | undefined;
+  pincode: string | undefined;
+  state: string | undefined;
+
+  gst: string | undefined;
+
+  contactPersonFirstName: string | undefined;
+  contactPersonMiddleName: string | undefined;
+  contactPersonLastName: string | undefined;
+  contactPersonPhoneNumber: string | undefined;
+
+  deviceName: string | undefined;
+  assetCode: string | undefined;
+  serviceTag: string | undefined;
+  issuedDate: string | undefined;
+
+  submit(form: NgForm) {
+    console.log("Form Submitted",form);
+    console.log(this.contactPersonPhoneNumber, this.issuedDate);
+  }
 }
